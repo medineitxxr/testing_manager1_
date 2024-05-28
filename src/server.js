54c2dae41697dc5f02dc41a12a12847b1513032f
medineitxxr <luismiguelmedina1975@gmail.com>
2024-05-27 20:07:50 -0500

    configuración inicial server npm run dev

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require ('express');
+
+const dotenv = require ('dotenv');
+
+dotenv.config();
+
+const app = require('./app');
+
+
+const PORT = process.env.PORT ||3000;
+app.listen(PORT,()=>{
+    console.log(`server listening on ${PORT}`);
+})
